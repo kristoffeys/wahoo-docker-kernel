2cbd4d5573900fca71bff53088ac01fd5ced6f73
Kristof Feys <kristof@ctibvba.be>
2023-04-17 17:55:53 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -14,7 +14,7 @@ git clone --depth=1 https://github.com/osm0sis/AnyKernel3.git
 echo
 echo "Cloning Kernel Repo"
 echo
-git clone --depth=1 --branch lineage-20 https://github.com/LineageOS/android_kernel_xiaomi_surya.git kernel
+git clone --depth=1 --branch lineage-20 https://github.com/LineageOS/android_kernel_google_wahoo.git kernel
 
 echo
 echo "Setting up env"
@@ -23,7 +23,7 @@ echo
 sudo apt-get install device-tree-compiler bc cpio ccache zip
 
 mkdir -p out
-export ZIPNAME=Surya-Kernel.zip
+export ZIPNAME=Wahoo-Kernel.zip
 export ARCH=arm64
 export SUBARCH=arm64
 export CLANG_PATH=/home/runner/work/Kernel-Actions/Kernel-Actions/clang/bin
@@ -37,7 +37,7 @@ echo
 echo "Moving to kernel dir"
 echo
 
-cp surya-docker_defconfig kernel/arch/arm64/configs/
+cp wahoo-docker_defconfig kernel/arch/arm64/configs/
 
 cd kernel
 echo $PWD
@@ -57,7 +57,7 @@ mkdir -p out
 echo
 echo "Set DEFCONFIG"
 echo 
-make CC=clang O=out surya-docker_defconfig
+make CC=clang O=out wahoo-docker_defconfig
 
 echo
 echo "Build The Good Stuff"
